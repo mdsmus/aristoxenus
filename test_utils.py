3f82f5a00d500c4c6b6a4ee7f5c364ca943a5804
Pedro Kroger <pedro.kroger@gmail.com>
2010-04-27 23:57:55 -0300

    fix test

--- test_utils.py
+++ test_utils.py
@@ -2,5 +2,5 @@ import utils
 
 
 def test_search_string():
-    assert utils.search_string("^foo", "foobar") == True
-    assert utils.search_string("[0-9]+", "foobar") == False
+    assert utils.search_string("^foo", "foobar") == "foo"
+    assert utils.search_string("[0-9]+", "foobar") == None
