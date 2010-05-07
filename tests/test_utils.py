bd2b55bbe16017bb3142881a526f0b8af86f9338
Pedro Kroger <pedro.kroger@gmail.com>
2010-05-07 20:22:06 -0300

    one assertion per test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,9 @@
 import utils
 
+# search_string
 
-def test_search_string():
+def test_search_string_foo_in_foobar():
     assert utils.search_string("^foo", "foobar") == "foo"
+
+def test_search_string_numbers_in_foobar():
     assert utils.search_string("[0-9]+", "foobar") == None
