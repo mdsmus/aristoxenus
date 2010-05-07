65e9ea3bad0049cedc2ffde3e5ca4a468487dd1d
Pedro Kroger <pedro.kroger@gmail.com>
2010-05-07 20:07:19 -0300

    one assertion per test, plus, remove useless code

--- tests/test_score.py
+++ tests/test_score.py
@@ -1,39 +1,54 @@
 import score
 from fractions import Fraction as frac
 
+# Simple tests to make sure the main classes are instantiating
+# correctly and without errors. Most classes will be tested in the
+# other tests in this suite, but it's good to have these simple tests
+# in case some class end up not being tested.
 
-def test_score():
-    """Simple tests to make sure the main classes are
-    instantiating correctly and without errors. Most classes will
-    be tested in the other tests in this suite, but it's good to
-    have these simple tests in case some class end up not being
-    tested.
-    """
+def pytest_funcarg__item_score(request):
+    sco = score.Score()
+    sco.append("foo")
+    return sco
 
-    s = score.Score()
-    s.append("foo")
 
-    record = score.Record("COM", "J. S. Bach")
-    comment = score.Comment("Foobar", 2)
-    tandem = score.Tandem("Clef", "C4")
-    exinterp = score.Exclusive("kern")
-    note = score.Note("c##", frac(1, 4))
-    score.octave = 6
-    score.code = 5
-    score.system = "base40"
-    score.type = "kern"
-    multiple_stop = score.MultipleStop()
-    bar = score.Bar(1)
-    rest = score.Rest(frac(1, 4))
+def test_score_append(item_score):
+    assert item_score == ["foo"]
 
+def test_score_is_a_score(item_score):
+    assert isinstance(item_score, score.Score)
+
+def test_score_is_a_list():
     assert score.Score(1, 2, 3) == [1, 2, 3]
-    assert s == ["foo"]
-    assert isinstance(s, score.Score)
+
+def test_score_record():
+    record = score.Record("COM", "J. S. Bach")
     assert isinstance(record, score.Record)
+
+def test_score_comment():
+    comment = score.Comment("Foobar", 2)
     assert isinstance(comment, score.Comment)
+
+def test_score_tandem():
+    tandem = score.Tandem("Clef", "C4")
     assert isinstance(tandem, score.Tandem)
+
+def test_score_exclusive():
+    exinterp = score.Exclusive("kern")
     assert isinstance(exinterp, score.Exclusive)
+
+def test_score_note():
+    note = score.Note("c##", frac(1, 4))
     assert isinstance(note, score.Note)
+
+def test_score_multiple_stop():
+    multiple_stop = score.MultipleStop()
     assert isinstance(multiple_stop, score.MultipleStop)
+
+def test_score_bar():
+    bar = score.Bar(1)
     assert isinstance(bar, score.Bar)
+
+def test_score_rest():
+    rest = score.Rest(frac(1, 4))
     assert isinstance(rest, score.Rest)
