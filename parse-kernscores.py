5650a0c1271149621f1dfddd9b1a86dc0bf128a7
Pedro Kroger <pedro.kroger@gmail.com>
2010-05-21 17:11:20 -0300

    script to parse all files in kernscore

---
+++ parse-kernscores.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import os.path
+
+from aristoxenus import parse
+from aristoxenus import emit
+from aristoxenus import music
+
+kernscoredir = "/home/kroger/Documents/kernscores"
+
+error_files = 0
+
+with open(os.path.join(kernscoredir, "filelist")) as filelist:
+    for line in filelist:
+        filename = os.path.join(kernscoredir, line.strip('\n'))
+        try:
+            parse.humdrum.parse_file(filename)
+        except (KeyError, parse.humdrum.kern.KernError, parse.humdrum.main.HumdrumError):
+            print("[ERROR]", filename)
+            error_files += 1
+
+print("{0} errors".format(error_files))
+
